8076f36ca7fbfb006d2f3d90cb4880e24d147480
brooke320 <opitz.brooke@gmail.com>
2020-10-23 10:36:07 -0500

    updated commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,7 +3,7 @@ let rollButton = document.querySelector("#roll-dice");
 let showAllRolls = document.querySelector("#show-all-rolls");
 let rollResults = document.querySelector("#roll-results");
 let total = document.querySelector("#total");
-let resetButton = document.querySelector("reset");
+let resetButton = document.querySelector("#reset");
 let dieRolls = [];
 
 //function randomNumber() {
@@ -20,18 +20,48 @@ rollButton.addEventListener("click", function () {
         dieRolls.push(randomNumber);
         count++;
     }
+
     //TOTAL
     function getSum(total, dieRolls) {
         return total + dieRolls
     }
     total.innerHTML += dieRolls.reduce(getSum);
 
-
-    //total.innerHTML = dieRolls.reduce((accumulator, ) => accumulator + currentValue
-    //);
-
-    //total.innerHTML += dieRolls.reduce((total, count) => total + count);
 })
+
+
+/*let dieRolls = [];
+
+let count = 0;
+
+while (count <= 50) {
+    dieRolls.push(Math.floor(Math.random() * 6) + 1);
+    count++;
+}
+
+let total = 0;
+let i = 0;
+
+while (i < dieRolls.length) {
+    console.log(dieRolls[i]);
+    total += dieRolls[i];
+    //will comtinue to update and add to total
+    i++;
+}
+
+total.innerHTML = "total" + total;
+
+console.log(dieRolls)
+console.log(total);
+
+//number 1 greater than last index b/c starts at 0 */
+
+
+//total.innerHTML = dieRolls.reduce((accumulator, ) => accumulator + currentValue
+//);
+
+//total.innerHTML += dieRolls.reduce((total, count) => total + count);*/
+
 
 
 
@@ -47,17 +77,13 @@ showAllRolls.addEventListener("click", function () {
 
 
 
-/*resetButton.addEventListener("click", function () {
-    function reset() {
-        rollDiceInput.value = 0;
-        rollResults.innerHTML = 0;
-        total.innerHTML = 0;
-        showAllRolls.innerHTML = 0;
-        rollButton.innerHTML = 0;
+resetButton.addEventListener("click", function reset() {
 
-        dieRolls = [];
-    }
-})*/
+    total.innerHTML = "Total:";
+    rollDiceInput.value = "";
+    rollResults.innerHTML = "";
+    dieRolls = [];
+})
 
 
 
